a1499eda9a6b2c913dc2db39075c8fb014363161
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-09 13:31:58 -0800

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -84,6 +84,19 @@ router.delete('./api/posts:id', (req,res) => {
         })
 })
 // [GET] /api/posts/:id/comments | Returns an array of all the comment objects associated with the post with the specified id
-// router.get('./api/posts/:id/comments' (req,res) => {})
+router.get('./api/posts/:id/comments', (req,res) => {
+    const {id} = req.params;
+    Post.findPostComments(id)
+        .then( post => {
+            if(!post){
+                res.status(404).json({message:"The post with the specified ID does not exist"})
+            }else{
+                res.status(200).json(post)
+             }
+        })
+        .catch( err => {
+            res.status(500).json({message:"The comments information could not be retrieved"})
+        })
+})
 
 module.exports = router;
